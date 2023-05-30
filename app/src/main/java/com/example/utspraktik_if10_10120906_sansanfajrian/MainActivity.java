215aced590f71ffb900d0e0f9e4fc1dece99c849
Sansan Fajrian <sansan.fajrian@gmail.com>
2023-05-30 20:03:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/utspraktik_if10_10120906_sansanfajrian/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.utspraktik_if10_10120906_sansanfajrian;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/**
+ * Tanggal Pengerjaan : 30/05/2023
+ * NIM : 10120906
+ * Nama : Sansan Fajrian
+ * Kelas : IF-10
+ * */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
